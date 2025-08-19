2143996787356d8e2844e7f89d63da0a35916c98
DeepakNagar1757 <deepnagar1757@gmail.com>
2025-08-19 15:22:00 +0530

    customeCursor Complited

--- dom manipulation using js/custom Cursor/script.js
+++ dom manipulation using js/custom Cursor/script.js
@@ -4,29 +4,47 @@ let shadowCursor = document.querySelector('.shadowCursor');
 let CursorX = 0;
 let CursorY = 0;
 
+// let shadowCursorX = 0;
+// let shadowCursorY = 0;
+
+
+let distanceX = 0;
+let distanceY = 0;
+
+let animationID = null;
+
+let timer = null;
+
+
+
 document.addEventListener('mousemove', (e) => {
     CursorX = e.clientX;
     CursorY = e.clientY;
     cursor.style.left = CursorX + 'px';
     cursor.style.top = CursorY + 'px';
-
-    animateCursor();
+    
+    if(!animationID){
+        animateCursor();
+    }
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+        cancelAnimationFrame(animationID)
+        animationID = null
+    }, 1000);
 });
 
+
 function animateCursor() {
+    let shadowCursorX = parseFloat(shadowCursor.style.left) || 0;
+    let shadowCursorY = parseFloat(shadowCursor.style.top) || 0;    
 
-    let shadowCursorX = shadowCursor.style.left || 0 ;
-    let shadowCursorY = shadowCursor.style.top || 0 ;
-    
-    let distanceX = CursorX - shadowCursorX;
-    let distanceY = CursorY - shadowCursorY;
+    distanceX = CursorX - shadowCursorX;
+    distanceY = CursorY - shadowCursorY;
 
-    console.log(distanceX, distanceY);
-    
+    shadowCursor.style.left = shadowCursorX + distanceX * 0.1 + 'px';
+    shadowCursor.style.top = shadowCursorY + distanceY * 0.1 + 'px';
+   
+    animationID = requestAnimationFrame(animateCursor)
+    console.log(animationID);
     
 }
-
-
-
-
-
